a455be1eb82af717d286ad3037d21c330356bacc
coli <kkwoo001021@naver.com>
2025-07-22 14:02:13 +0900

    feat: 오디세이 response mapper 사용

---
+++ src/main/java/com/devoops/client/OdsayRouteClient.java
@@ -0,0 +1,59 @@
+package com.devoops.client;
+
+import com.devoops.dto.OdsayResponse;
+import com.devoops.exception.OdsayUtilException;
+import com.devoops.mapper.OdsayResponseMapper;
+import com.devoops.vo.Coordinates;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@RequiredArgsConstructor
+public class OdsayRouteClient {
+
+    private final String apiKey;
+    private final RestClient restClient;
+
+    public OdsayRouteClient(String apiKey, RestClient.Builder builder) {
+        this(apiKey, builder.build());
+    }
+
+    public long calculateRouteMinutes(Coordinates origin, Coordinates target) {
+        OdsayResponse response = getOdsayResponse(origin, target);
+        return responseToRouteTime(response);
+    }
+
+    private OdsayResponse getOdsayResponse(Coordinates origin, Coordinates target) {
+        OdsayResponse response = restClient.get()
+                .uri(makeURI(origin, target))
+                .retrieve()
+                .body(OdsayResponse.class);
+        return Objects.requireNonNullElseGet(response, () -> {
+            throw new OdsayUtilException("서버 에러");
+        });
+    }
+
+    private URI makeURI(Coordinates origin, Coordinates target) {
+        String uri = "https://api.odsay.com/v1/api/searchPubTransPathT"
+                + "?SX=" + origin.getLongitude()
+                + "&SY=" + origin.getLatitude()
+                + "&EX=" + target.getLongitude()
+                + "&EY=" + target.getLatitude()
+                + "&apiKey=" + apiKey;
+        try {
+            return new URI(uri);
+        } catch (URISyntaxException exception) {
+            throw new OdsayUtilException(exception.getMessage());
+        }
+    }
+
+    private long responseToRouteTime(OdsayResponse response) {
+        return OdsayResponseMapper.mapMinutes(response);
+    }
+}
+
---
+++ src/main/java/com/devoops/mapper/OdsayResponseMapper.java
@@ -0,0 +1,53 @@
+package com.devoops.mapper;
+
+import com.devoops.dto.OdsayResponse;
+import com.devoops.exception.OdsayUtilException;
+import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OdsayResponseMapper {
+
+    private static final String CLOSE_LOCATION_CODE = "-98"; //출발지-도착지가 700m 이내일 때
+    private static final String ODSAY_SERVER_ERROR = "500";
+
+    public static long mapMinutes(OdsayResponse response) {
+        if (response == null) {
+            throw new OdsayUtilException("오디세이로부터 온 응답이 null입니다");
+        }
+
+        if (isCloseLocation(response)) {
+            return -1;
+        }
+
+        if (response.code().isPresent()) {
+            checkOdsayException(response);
+        }
+
+        return response.minutes()
+                .orElseThrow(() -> new OdsayUtilException("OdsayResponse minutes is Empty"));
+    }
+
+    private static boolean isCloseLocation(OdsayResponse response) {
+        Optional<String> code = response.code();
+        return code.isPresent() && CLOSE_LOCATION_CODE.equals(code.get());
+    }
+
+    private static void checkOdsayException(OdsayResponse response) {
+        if (isServerErrorCode(response)) {
+            log.error("ODsay Server Error: {}", response);
+            throw new OdsayUtilException("오디세이 서버 에러");
+        }
+
+        throw new OdsayUtilException("ODSay BAD REQUEST Error: " + response);
+    }
+
+    private static boolean isServerErrorCode(OdsayResponse response) {
+        Optional<String> code = response.code();
+        return code.isPresent() && ODSAY_SERVER_ERROR.equals(code.get());
+    }
+}
+
