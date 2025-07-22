beaa7b0e7f0eb26d0a111215e5ad4ef13abeb2b3
coli <kkwoo001021@naver.com>
2025-07-22 14:03:33 +0900

    feat: 오디세이 400에러 반환

---
+++ src/main/java/com/devoops/exception/OdsayBadRequestException.java
@@ -0,0 +1,8 @@
+package com.devoops.exception;
+
+public class OdsayBadRequestException extends RuntimeException {
+
+    public OdsayBadRequestException(String message) {
+        super(message);
+    }
+}
